e7c887ba4017faa3213542e592b459ee82b0e148
Tony <waiting33118@gmail.com>
2020-07-01 15:10:56 +0800

    refactor: router setting

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const exphdbs = require('express-handlebars')
 
-const routes = require('./routes')
+const useRoutes = require('./routes')
 
 const app = express()
 const PORT = 3000
 
 app.engine('handlebars', exphdbs())
 app.set('view engine', 'handlebars')
-app.use(routes)
 
 app.listen(PORT, () => console.log(`The server is running on PORT:${PORT}`))
+useRoutes(app)
