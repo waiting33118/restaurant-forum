9153e69cea4c1db822cda4ab3f1c5f6a2bafac57
Tony <waiting33118@gmail.com>
2020-07-02 14:46:14 +0800

    feat: add signin function

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,20 @@ const restController = require('../controllers/restController')
 const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
 
-module.exports = (app) => {
+module.exports = (app, passport) => {
   app.get('/', (req, res) => res.redirect('/restaurants'))
   app.get('/restaurants', restController.getRestaurants)
 
   app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
   app.get('/admin/restaurants', adminController.getRestaurants)
 
-  app.get('/signup', userController.signupPage)
-  app.post('/signup', userController.signup)
+  app.get('/signup', userController.signUpPage)
+  app.get('/signin', userController.signInPage)
+  app.get('/logout', userController.logout)
+  app.post('/signup', userController.signUp)
+  app.post(
+    '/signin',
+    passport.authenticate('local', { failureRedirect: '/signin' }),
+    userController.signIn
+  )
 }
