94bdc71aaa29427e41de5c6c8e6e96df66c15a08
Tony <waiting33118@gmail.com>
2020-07-02 14:45:20 +0800

    feat: init & use passport

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const flash = require('connect-flash')
 
 const db = require('./models')
 const useRoutes = require('./routes')
+const passport = require('./config/passport')
 
 const app = express()
 const PORT = 3000
@@ -16,14 +17,17 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(
   session({ secret: 'ThisIsMySecret', resave: false, saveUninitialized: false })
 )
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
   next()
 })
 
 app.listen(PORT, () => {
   console.log(`The server is running on PORT:${PORT}`)
 })
-useRoutes(app)
+useRoutes(app, passport)
