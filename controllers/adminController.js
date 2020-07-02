e7db5e351240f8caa63ee3014547b272a29821d1
Tony <waiting33118@gmail.com>
2020-07-02 21:21:39 +0800

    feat: add Admin control functions

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,35 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+
 const adminController = {
   getRestaurants: (req, res) => {
-    res.render('admin/home')
+    Restaurant.findAll({ raw: true, nest: true })
+      .then((restaurants) => {
+        res.render('admin/home', { restaurants })
+      })
+      .catch((error) => console.log(error))
+  },
+  createRestaurant: (req, res) => {
+    res.render('admin/create')
+  },
+  postRestaurants: (req, res) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) {
+      req.flash('error_messages', '名稱為必填！')
+      return res.redirect('back')
+    }
+    Restaurant.create({
+      name,
+      tel,
+      address,
+      openingHours,
+      description
+    })
+      .then((restaurant) => {
+        req.flash('success_messages', `已成功建立"${restaurant.name}"餐廳！`)
+        res.redirect('/admin/restaurants')
+      })
+      .catch((error) => console.log(error))
   }
 }
 
