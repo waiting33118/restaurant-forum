57c3654413fee774e62bc0ced95fc5fb8e0ea306
Tony <waiting33118@gmail.com>
2020-07-02 10:15:25 +0800

    feat: init session & flash messages

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const exphdbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 const db = require('./models')
 const useRoutes = require('./routes')
@@ -9,9 +12,18 @@ const PORT = 3000
 
 app.engine('handlebars', exphdbs())
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(
+  session({ secret: 'ThisIsMySecret', resave: false, saveUninitialized: false })
+)
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(PORT, () => {
-  db.sequelize.sync()
   console.log(`The server is running on PORT:${PORT}`)
 })
 useRoutes(app)
