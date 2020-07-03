a94bdb075ec1561c525d0115b5215110a162e435
Tony <waiting33118@gmail.com>
2020-07-03 12:54:41 +0800

    feat: edit variable name

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Restaurant extends Model {
     /**
@@ -12,16 +10,19 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  Restaurant.init({
-    name: DataTypes.STRING,
-    tel: DataTypes.STRING,
-    address: DataTypes.STRING,
-    opening_hours: DataTypes.STRING,
-    description: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'Restaurant',
-  });
-  return Restaurant;
-};
+  }
+  Restaurant.init(
+    {
+      name: DataTypes.STRING,
+      tel: DataTypes.STRING,
+      address: DataTypes.STRING,
+      openingHours: DataTypes.STRING,
+      description: DataTypes.TEXT
+    },
+    {
+      sequelize,
+      modelName: 'Restaurant'
+    }
+  )
+  return Restaurant
+}
