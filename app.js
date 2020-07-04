f763142902c81742eb501cbf763c4f0d5f07322a
Tony <waiting33118@gmail.com>
2020-07-04 13:59:43 +0800

    feat: add production PORT

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const useRoutes = require('./routes')
 const passport = require('./config/passport')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.engine('handlebars', exphdbs())
 app.set('view engine', 'handlebars')
