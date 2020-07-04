611f88029dfd20b9cdd578a15d90a42e12da3e40
Tony <waiting33118@gmail.com>
2020-07-04 11:02:39 +0800

    feat: use multer & fs to upload images

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const db = require('../models')
 const Restaurant = db.Restaurant
 
@@ -22,22 +23,49 @@ const adminController = {
   },
   postRestaurants: (req, res) => {
     const { name, tel, address, openingHours, description } = req.body
+    const { file } = req
+
     if (!name) {
       req.flash('error_messages', '名稱為必填！')
       return res.redirect('back')
     }
-    Restaurant.create({
-      name,
-      tel,
-      address,
-      openingHours,
-      description
-    })
-      .then((restaurant) => {
-        req.flash('success_messages', `已成功建立"${restaurant.name}"餐廳！`)
-        res.redirect('/admin/restaurants')
+    if (file) {
+      fs.readFile(file.path, (err, data) => {
+        if (err) console.log('Error', err)
+        fs.writeFile(`upload/${file.originalname}`, data, () => {
+          Restaurant.create({
+            name,
+            tel,
+            address,
+            openingHours,
+            description,
+            image: file ? `/upload/${file.originalname}` : null
+          })
+            .then((restaurant) => {
+              req.flash(
+                'success_messages',
+                `已成功建立"${restaurant.name}"餐廳！`
+              )
+              res.redirect('/admin/restaurants')
+            })
+            .catch((error) => console.log(error))
+        })
       })
-      .catch((error) => console.log(error))
+    } else {
+      Restaurant.create({
+        name,
+        tel,
+        address,
+        openingHours,
+        description,
+        image: null
+      })
+        .then((restaurant) => {
+          req.flash('success_messages', `已成功建立"${restaurant.name}"餐廳！`)
+          res.redirect('/admin/restaurants')
+        })
+        .catch((error) => console.log(error))
+    }
   },
   editRestaurant: (req, res) => {
     const { id } = req.params
@@ -48,28 +76,60 @@ const adminController = {
   putRestaurant: (req, res) => {
     const { name, tel, address, openingHours, description } = req.body
     const { id } = req.params
+    const { file } = req
+
     if (!name) {
       req.flash('error_messages', '名稱為必填！')
       return res.redirect('back')
     }
-    Restaurant.findByPk(id, { raw: true })
-      .then(() => {
-        Restaurant.update(
-          {
-            name,
-            tel,
-            address,
-            openingHours,
-            description
-          },
-          { where: { id } }
-        ).catch((error) => console.log(error))
+    if (file) {
+      fs.readFile(file.path, (err, data) => {
+        if (err) console.log('Error', err)
+        fs.writeFile(`upload/${file.originalname}`, data, () => {
+          Restaurant.findByPk(id, { raw: true })
+            .then((restaurant) => {
+              Restaurant.update(
+                {
+                  name,
+                  tel,
+                  address,
+                  openingHours,
+                  description,
+                  image: file
+                    ? `/upload/${file.originalname}`
+                    : restaurant.image
+                },
+                { where: { id } }
+              ).catch((error) => console.log(error))
+            })
+            .then(() => {
+              req.flash('success_messages', '已成功修改餐廳明細！')
+              res.redirect('/admin/restaurants')
+            })
+            .catch((error) => console.log(error))
+        })
       })
-      .then(() => {
-        req.flash('success_messages', '已成功修改餐廳明細！')
-        res.redirect('/admin/restaurants')
-      })
-      .catch((error) => console.log(error))
+    } else {
+      Restaurant.findByPk(id, { raw: true })
+        .then((restaurant) => {
+          Restaurant.update(
+            {
+              name,
+              tel,
+              address,
+              openingHours,
+              description,
+              image: restaurant.image
+            },
+            { where: { id } }
+          ).catch((error) => console.log(error))
+        })
+        .then(() => {
+          req.flash('success_messages', '已成功修改餐廳明細！')
+          res.redirect('/admin/restaurants')
+        })
+        .catch((error) => console.log(error))
+    }
   },
   deleteRestaurant: (req, res) => {
     const { id } = req.params
