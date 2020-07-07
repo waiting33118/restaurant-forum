3e86adf6a924c78077b8f83ffc73276b80b3727c
Tony <waiting33118@gmail.com>
2020-07-07 09:19:03 +0800

    feat: create render restaurnts Page UI & restaurnt Page feature

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,29 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
 const restController = {
   getRestaurants: (req, res) => {
-    res.render('home')
+    Restaurant.findAll({ raw: true, nest: true, include: [Category] })
+      .then((restaurants) => {
+        const data = restaurants.map((r) => ({
+          ...r,
+          description: r.description.substring(0, 50),
+          categoryName: r.Category.name
+        }))
+        res.render('restaurants', { restaurants: data })
+      })
+      .catch((error) => console.log(error))
+  },
+  getRestaurant: (req, res) => {
+    const { id } = req.params
+    Restaurant.findByPk(id, {
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+      .then((restaurant) => res.render('restaurant', { restaurant }))
+      .catch((error) => console.log(error))
   }
 }
 
