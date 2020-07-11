00180d842e992118eddf28549fda1e2e8f0c2fa1
Tony <waiting33118@gmail.com>
2020-07-11 14:29:26 +0800

    feat: create followship models &  associate followerId,followingId

--- models/user.js
+++ models/user.js
@@ -20,6 +20,16 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'UserId',
         as: 'LikedRestaurants'
       })
+      User.belongsToMany(models.User, {
+        through: models.Followship,
+        foreignKey: 'followingId',
+        as: 'Followers'
+      })
+      User.belongsToMany(models.User, {
+        through: models.Followship,
+        foreignKey: 'followerId',
+        as: 'Followings'
+      })
     }
   }
   User.init(
