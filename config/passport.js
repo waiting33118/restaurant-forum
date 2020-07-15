c2ff5a03ff1d04e8ddd9ce5979e4aad02bd1c512
Tony <waiting33118@gmail.com>
2020-07-15 11:12:37 +0800

    feat: use jwt passport & add auth to each routes

--- config/passport.js
+++ config/passport.js
@@ -1,9 +1,17 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcryptjs')
+const passportJWT = require('passport-jwt')
+const ExtractJwt = passportJWT.ExtractJwt
+const JwtStrategy = passportJWT.Strategy
+
 const db = require('../models')
 const User = db.User
 const Restaurant = db.Restaurant
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
+}
 
 passport.use(
   new LocalStrategy(
@@ -48,5 +56,21 @@ passport.deserializeUser((id, done) => {
     })
     .catch((error) => console.log(error))
 })
+
+passport.use(
+  new JwtStrategy(jwtOptions, (jwt_payload, next) => {
+    User.findByPk(jwt_payload.id, {
+      include: [
+        { model: Restaurant, as: 'FavoritedRestaurants' },
+        { model: Restaurant, as: 'LikedRestaurants' },
+        { model: User, as: 'Followers' },
+        { model: User, as: 'Followings' }
+      ]
+    }).then(user => {
+      if (!user) return next(null, false)
+      return next(null, user)
+    }).catch(error => console.log(error))
+  })
+)
 
 module.exports = passport
--- routes/apis.js
+++ routes/apis.js
@@ -2,23 +2,33 @@ const express = require('express')
 const router = express.Router()
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
+const passport = require('../config/passport')
 
+const authenticated = passport.authenticate('jwt', { session: false })
+const authenticatedAdmin = (req, res, next) => {
+  if (req.user) {
+    if (req.user.isAdmin) return next()
+    return res.json({ status: 'error', message: 'permission denied' })
+  } else {
+    return res.json({ status: 'error', message: 'permission denied' })
+  }
+}
 const adminController = require('../controllers/api/adminController')
 const userController = require('../controllers/api/userController')
 const categoryController = require('../controllers/api/categoryController')
 
 // admin
-router.get('/admin/restaurants', adminController.getRestaurants)
-router.get('/admin/restaurant/:id', adminController.getRestaurant)
-router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurants)
-router.put('/admin/restaurant/:id', upload.single('image'), adminController.putRestaurant)
-router.delete('/admin/restaurant/:id', adminController.deleteRestaurant)
+router.get('/admin/restaurants', authenticated, authenticatedAdmin, adminController.getRestaurants)
+router.get('/admin/restaurant/:id', authenticated, authenticatedAdmin, adminController.getRestaurant)
+router.post('/admin/restaurants', authenticated, authenticatedAdmin, upload.single('image'), adminController.postRestaurants)
+router.put('/admin/restaurant/:id', authenticated, authenticatedAdmin, upload.single('image'), adminController.putRestaurant)
+router.delete('/admin/restaurant/:id', authenticated, authenticatedAdmin, adminController.deleteRestaurant)
 
 // category
-router.get('/admin/categories', categoryController.getCategories)
-router.post('/admin/categories', categoryController.postCategories)
-router.put('/admin/categories/:id', categoryController.putCategories)
-router.delete('/admin/categories/:id', categoryController.deleteCategories)
+router.get('/admin/categories', authenticated, authenticatedAdmin, categoryController.getCategories)
+router.post('/admin/categories', authenticated, authenticatedAdmin, categoryController.postCategories)
+router.put('/admin/categories/:id', authenticated, authenticatedAdmin, categoryController.putCategories)
+router.delete('/admin/categories/:id', authenticated, authenticatedAdmin, categoryController.deleteCategories)
 
 // JWT signin
 router.post('/signin', userController.signIn)
